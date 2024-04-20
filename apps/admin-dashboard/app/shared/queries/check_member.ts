686170eca75aa50c097c9e91cb3ea2f90c5df2a5
tomas-salgado <tas268@corenll.edu>
2024-04-20 10:37:02 -0400

    test

--- apps/admin-dashboard/app/shared/queries/check_member.ts
+++ apps/admin-dashboard/app/shared/queries/check_member.ts
@@ -1,4 +1,5 @@
-import { db } from '../core.server';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { db } = require('../core.server');
 
 export async function StudentHasGithub(githubId: string): Promise<boolean> {
   const result = await db
